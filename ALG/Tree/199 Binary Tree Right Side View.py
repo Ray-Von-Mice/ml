8a407c40a5e87a88cdde61e9711461480fdb3219
Ray Zhang <rayvonmicesy@yahoo.com>
2026-02-23 15:48:30 -0500

    LT199

---
+++ ALG/Tree/199 Binary Tree Right Side View.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        travel, result = deque([root]), []
+        while travel:
+            n, level = len(travel), []
+            for _ in range(n):
+                curr = travel.popleft()
+                if curr.left:
+                    travel.append(curr.left)
+                if curr.right:
+                    travel.append(curr.right)
+                level.append(curr.val)
+            result.append(level[-1])
+        return result
