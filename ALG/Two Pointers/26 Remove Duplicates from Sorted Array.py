a0790ed74a9abd926424ef29a889bcbb3ea1c64e
Ray Zhang <rayvonmicesy@yahoo.com>
2026-03-02 16:27:16 -0500

    add comment

--- ALG/Two Pointers/26 Remove Duplicates from Sorted Array.py
+++ ALG/Two Pointers/26 Remove Duplicates from Sorted Array.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        cur = 0
+        cur = 0 #index that diff number is being coping to
         for num in nums:
             if num != nums[cur - 1] or cur < 1:
                 nums[cur] = num
