e1a728665103f62dd2beb592a5d35315a84783d2
Ray Zhang <rayvonmicesy@yahoo.com>
2026-03-03 13:25:14 -0500

    LT79 error correction

--- ALG/Backtracking/79 Word Search set.py
+++ ALG/Backtracking/79 Word Search set.py
@@ -19,7 +19,7 @@ class Solution:
             res3 = dfs(r - 1, c, indx + 1)
             res4 = dfs(r, c - 1, indx + 1)
             visit.remove((r, c))
-            return (res1 or res2 or res2 or res4)
+            return (res1 or res2 or res3 or res4)
         
         for i in range(m):
             for j in range(n):
