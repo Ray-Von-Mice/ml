804aba9ef8d11ab72c0e22b267e7a91b1a09dcb1
Ray Zhang <rayvonmicesy@yahoo.com>
2026-03-13 23:43:56 -0400

    LT518 add comments

--- ALG/2D Dynamic Programming/518 Coin Change II Topdown.py
+++ ALG/2D Dynamic Programming/518 Coin Change II Topdown.py
@@ -2,23 +2,25 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
         coins.sort()
-        solutions = [[-1] * (amount + 1) for _ in range(n + 1)]
+        # for each coin, we have 0 to amount numbers we can reach; memo[i][num] how many ways we can reach num using coins[i]
+        memo = [[-1] * (amount + 1) for _ in range(n)]
 
-        def dfs(indx, total):
-            if total == 0:
+        def dfs(indx, remain):
+            if remain == 0:
                 return 1
-            if indx >= n:
+            if indx >= n or remain < 0:
                 return 0
+            if memo[indx][remain] != -1:
+                return memo[indx][remain]
             
-            if solutions[indx][total] != -1:
-                return solutions[indx][total]
+            count = 0
+            if remain - coins[indx] >= 0:
+                # skip current index, move to next coin
+                count += dfs(indx + 1, remain)
+                # pick current indx, remain deduct current coin value
+                count += dfs(indx, remain - coins[indx])
             
-            result = 0
-            if total >= coins[indx]:
-                result += dfs(indx + 1, total)
-                result += dfs(indx, total - coins[indx])
-            
-            solutions[indx][total] = result
-            return result
-
+            memo[indx][remain] = count
+            return count
+        
         return dfs(0, amount)
